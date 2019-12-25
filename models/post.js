533a7f4671734765f10fa3f7786ba18ca2205859
Riccardo Lazzari <riclazza@gmail.com>
2019-12-25 12:55:09 +0100

    correct indentation

--- models/post.js
+++ models/post.js
@@ -1,23 +1,23 @@
 var mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
 var postSchema = new mongoose.Schema({
-    title: String,
-    image: String,
-    body: String,
-    created: {type: Date, default: Date.now},
-    author: {
-        id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User'
-        },
-        username: String
+  title: String,
+  image: String,
+  body: String,
+  created: { type: Date, default: Date.now },
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
     },
-    comments: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Comment'
-        }
-    ]
+    username: String
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment'
+    }
+  ]
 });
 var Post = mongoose.model('Post', postSchema);
 
--- routes/posts.js
+++ routes/posts.js
@@ -5,104 +5,104 @@ var Comment = require('../models/comment');
 var middleware = require('../middleware');
 
 //Root route
-router.get('/', function(req, res){
-    res.redirect('/posts');
+router.get('/', function(req, res) {
+  res.redirect('/posts');
 });
 
-
 //INDEX route
-router.get('/posts', function(req, res){
-    //find every post in database
-    Post.find({}, function(err, allPosts){
-        if(err){
-            console.log("ERROR");
-        } else {
-            res.render('posts/index', {posts: allPosts});
-        }
-    })
+router.get('/posts', function(req, res) {
+  //find every post in database
+  Post.find({}, function(err, allPosts) {
+    if (err) {
+      console.log('ERROR');
+    } else {
+      res.render('posts/index', { posts: allPosts });
+    }
+  });
 });
 
 //NEW route
-router.get('/posts/new', middleware.isLoggedIn, function(req, res){
-    res.render('posts/new');
+router.get('/posts/new', middleware.isLoggedIn, function(req, res) {
+  res.render('posts/new');
 });
 
 //CREATE route
-router.post('/posts', middleware.isLoggedIn, function(req, res){
-    //sanitize the input
-    req.body.post.body = req.sanitize(req.body.post.body);
-    //take the author and put it in the Post
-    req.body.post.author = {
-        id: req.user._id,
-        username: req.user.username
+router.post('/posts', middleware.isLoggedIn, function(req, res) {
+  //sanitize the input
+  req.body.post.body = req.sanitize(req.body.post.body);
+  //take the author and put it in the Post
+  req.body.post.author = {
+    id: req.user._id,
+    username: req.user.username
+  };
+  //create Post
+  Post.create(req.body.post, function(err, newPost) {
+    if (err) {
+      res.render('posts/new');
+    } else {
+      //redirect to index
+      res.redirect('/posts');
     }
-    //create Post
-    Post.create(req.body.post, function(err, newPost){
-        if (err) {
-            res.render('posts/new');
-        } else {
-            //redirect to index
-            res.redirect('/posts');
-        }    
-    });  
+  });
 });
 
 //SHOW route
-router.get('/posts/:id', function(req, res){
-    Post.findById(req.params.id).populate('comments').exec(function(err, foundPost){
-        if (err || !foundPost) { //check also if foundPost is null. !null is true
-            req.flash('error', 'Post not found');
-            res.redirect('/posts');
-        } else {
-            res.render('posts/show', {post: foundPost});
-        }
+router.get('/posts/:id', function(req, res) {
+  Post.findById(req.params.id)
+    .populate('comments')
+    .exec(function(err, foundPost) {
+      if (err || !foundPost) {
+        //check also if foundPost is null. !null is true
+        req.flash('error', 'Post not found');
+        res.redirect('/posts');
+      } else {
+        res.render('posts/show', { post: foundPost });
+      }
     });
-    
 });
 
 //EDIT route
-router.get('/posts/:id/edit', middleware.checkPostOwnership, function(req, res){
-    //checking if user is logged 
-    Post.findById(req.params.id, function(err, foundPost){
-        res.render('posts/edit', {post: foundPost});
-    });
-   
-    
+router.get('/posts/:id/edit', middleware.checkPostOwnership, function(
+  req,
+  res
+) {
+  //checking if user is logged
+  Post.findById(req.params.id, function(err, foundPost) {
+    res.render('posts/edit', { post: foundPost });
+  });
 });
 
 //UPDATE route
-router.put('/posts/:id', middleware.checkPostOwnership, function(req, res){
+router.put('/posts/:id', middleware.checkPostOwnership, function(req, res) {
+  req.body.post.body = req.sanitize(req.body.post.body);
 
-    req.body.post.body = req.sanitize(req.body.post.body);
-
-    Post.findByIdAndUpdate(req.params.id, req.body.post, function(err, updatedPost){
-        if (err) {
-            res.redirect('/posts');
-        } else {
-            res.redirect('/posts/' + req.params.id);
-        }
-    });
+  Post.findByIdAndUpdate(req.params.id, req.body.post, function(
+    err,
+    updatedPost
+  ) {
+    if (err) {
+      res.redirect('/posts');
+    } else {
+      res.redirect('/posts/' + req.params.id);
+    }
+  });
 });
-
 
 //DELETE route
-router.delete('/posts/:id', middleware.checkPostOwnership, function(req, res){
-    //destroy Post and redirect 
-    Post.findByIdAndRemove(req.params.id, function(err, removedPost){
-        if (err) {
-            console.log(err);
-        }
-        //delete all the comments related to the post that we are removing
-        Comment.deleteMany( {_id: { $in: removedPost.comments} }, function(err){
-            if (err) {
-                console.log(err);
-            }
-        });
-        res.redirect('/posts');
-        
+router.delete('/posts/:id', middleware.checkPostOwnership, function(req, res) {
+  //destroy Post and redirect
+  Post.findByIdAndRemove(req.params.id, function(err, removedPost) {
+    if (err) {
+      console.log(err);
+    }
+    //delete all the comments related to the post that we are removing
+    Comment.deleteMany({ _id: { $in: removedPost.comments } }, function(err) {
+      if (err) {
+        console.log(err);
+      }
     });
+    res.redirect('/posts');
+  });
 });
-
-
 
 module.exports = router;
