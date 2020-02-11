5fbe7a9c43b07d4ce865bbd58ff8129bca790d6f
Riccardo Lazzari <r.lazzari@archimediaweb.it>
2020-02-11 16:54:00 +0100

    added async/await in authController and changed layout of the login and register pages

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,34 +1,44 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
-exports.checkPostOwnership = (req, res, next) => {
-  const foundPost = Post.findById(req.params.id);
-  if (!foundPost) {
-    req.flash('error', 'Post not found');
-    res.redirect('back');
-  }
-  if (foundPost.author.id.equals(req.user._id)) {
-    next();
-  } else {
-    req.flash('error', "You don't have permission to do that");
-    res.redirect('back');
+exports.checkPostOwnership = async (req, res, next) => {
+  try {
+    const foundPost = await Post.findById(req.params.id);
+    if (!foundPost) {
+      req.flash('error', 'Post not found');
+      res.redirect('back');
+    }
+    if (foundPost.author.id.equals(req.user._id)) {
+      next();
+    } else {
+      req.flash('error', "You don't have permission to do that");
+      res.redirect('back');
+    }
+  } catch (err) {
+    req.flash('error', err.message);
+    res.redirect('/posts');
   }
 };
 
-exports.checkCommentOwnership = (req, res, next) => {
+exports.checkCommentOwnership = async (req, res, next) => {
   //user is the author of the comment
   //foundComment.author.id  is a mongoose obj
   //req.user._id  is a string
-  const foundComment = Comment.findById(req.params.comment_id);
-  if (!foundComment) {
-    req.flash('error', 'Comment not found');
-    res.redirect('back');
-  }
-  if (foundComment.author.id.equals(req.user._id)) {
-    next();
-  } else {
-    req.flash('error', "You don't have permission to do that");
-    res.redirect('back');
+  try {
+    const foundComment = await Comment.findById(req.params.comment_id);
+    if (!foundComment) {
+      req.flash('error', 'Comment not found');
+      res.redirect('back');
+    }
+    if (foundComment.author.id.equals(req.user._id)) {
+      next();
+    } else {
+      req.flash('error', "You don't have permission to do that");
+      res.redirect('back');
+    }
+  } catch (err) {
+    req.flash('error', err.message);
+    res.redirect('/posts');
   }
 };
 
